48e251cd657c993a8375c2576c92ae998577366d
Cheshire-dev <leonardobang123carvalho@gmaail.com>
2021-05-09 20:16:52 -0400

    First Request

--- frontend/src/components/DonuntChart/index.tsx
+++ frontend/src/components/DonuntChart/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
@@ -11,9 +12,14 @@ const DonutChart = () =>{
 
     let chartData : ChartData = {labels: [], series: []};
 
-    axios.get(`${BASE_URL}'/sales/amount-by-seller'`)
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then(response => {
-            console.log(response.data); 
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName)
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels: myLabels, series: mySeries};
+            console.log(chartData); 
         });
 
     //const mockData = {
